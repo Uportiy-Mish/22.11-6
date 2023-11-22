823b48abf710e0c2a42e64375c7a661a8f7d7291
Asus <Asus@DESKTOP-Q1VVDVK>
2023-11-22 15:28:33 +0300

    Добавьте файлы проекта.

---
+++ 22.11 Практика Задани№6/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _22._11_практика_задание_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string year1;
+            year1 = Interaction.InputBox("Введите текущий год",
+                "Ввод года");
+            string year2;
+            year2 = Interaction.InputBox("Введите ваш год рождения",
+                "Ввод вашего года рождения");
+            int x = Convert.ToInt32(year1);
+            int y = Convert.ToInt32(year2);
+            int age = x - y;
+            string res = age.ToString();
+
+
+            //string txt = "Имя: " + name1 + ", Фамилия: " + name2;
+            MessageBox.Show(res, "Итог.");
+        }
+    }
+}
---
+++ 22.11 Практика Задани№6/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("22.11 Практика Задани№6")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("22.11 Практика Задани№6")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("8e9364db-1f58-451c-8bb2-164b4bd43055")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
